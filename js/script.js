bfef164153758aef2bede6c699dd431cd9fef6f4
Davud77 <77davud77@gmail.com>
2022-07-29 17:52:54 +0300

    upd carusel

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 100,
-        adaptiveHeight: true
-    }
-    );
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+
+
+(document).querySelector('.prev').addEventListener('click', function(){
+    slider.goTo('prev');  
 });
+
+(document).querySelector('.next').addEventListener('click', function(){
+    slider.goTo('next');  
+});
+
+
+    //modal
+
+    /* $('[data-modal=consultations]').on('click',function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal_close').on('click', function() {
+        $('.overlay, #consultation, #thinks, #order').fadeOut('slow');
+    });
+    $('.button_mini').on('click', function() {
+        $('.overlay, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('');
+        });
+    }); */
+
